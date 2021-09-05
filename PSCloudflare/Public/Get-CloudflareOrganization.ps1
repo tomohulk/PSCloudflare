1ced3e65d8b7fab77ba4b3903bde5807aebf0560
Thomas Malkewitz <thomas.malkewitz@icloud.com>
2021-09-05 17:24:52 -0400

    Added Get-CloudflareOrganization cmdlet.

--- PSCloudflare/Public/Get-CloudflareOrganization.ps1
+++ PSCloudflare/Public/Get-CloudflareOrganization.ps1
@@ -22,6 +22,12 @@ Function Get-CloudflareOrganization {
     Process {
         $endpoint = 'user/organizations'
 
+        if (($PSBoundParameters.GetEnumerator().Where({ $_.Key -ne 'RawResponse' })).Count -ne 0) {
+            $parameterList = [Hashtable]$PSBoundParameters
+
+            $endpoint += Format-CloudflareEndpointString -ParameterList $parameterList
+        }
+
         $response = Invoke-CloudflareAPI -Method GET -Endpoint $endpoint
 
         Write-CloudflareResponse -Response $response -CloudflareObjectType 'CloudflareOrganization' -RawResponse $RawResponse.IsPresent
